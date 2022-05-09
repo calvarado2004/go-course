eed4520809499efbf454eb08139998a06270ce56
Carlos Alvarado <carlos-alvarado@outlook.com>
2022-05-08 20:23:06 -0400

    static files

--- bookings/cmd/web/routes.go
+++ bookings/cmd/web/routes.go
@@ -25,5 +25,9 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/hello-world", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	fileServer := http.FileServer(http.Dir("./static/"))
+
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
