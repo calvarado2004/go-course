7f0822cf0c02c3ff23ac869a6bb1f160bbd1fa48
Carlos Alvarado <carlos-alvarado@outlook.com>
2022-05-28 10:50:00 -0400

    Added more tests

--- bookings/cmd/web/main.go
+++ bookings/cmd/web/main.go
@@ -21,6 +21,25 @@ var session *scs.SessionManager
 
 func main() {
 
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+
+}
+
+func run() error {
+
 	//what we are going to put in the session
 	gob.Register(models.Reservation{})
 
@@ -38,6 +57,7 @@ func main() {
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
+		return err
 	}
 
 	app.TemplateCache = tc
@@ -49,14 +69,5 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
-
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-	log.Fatal(err)
-
+	return nil
 }
--- bookings/go.mod
+++ bookings/go.mod
@@ -8,3 +8,5 @@ require (
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/justinas/nosurf v1.1.1
 )
+
+require github.com/go-chi/chi v1.5.4 // indirect
